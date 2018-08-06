b5b53ddcea70b8401526f8d3281fccbd5ed89cb9
Jell PD <>
2018-08-07 01:21:56 +0900

    change title.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { app, BrowserWindow, ipcMain } from "electron";
 let onlineStatusWindow: BrowserWindow
 
 app.on('ready', () => {
-  onlineStatusWindow = new BrowserWindow({ width: 800, height: 600, show: true})
+  onlineStatusWindow = new BrowserWindow({ title: 'Is Jell\'s blog alive?', width: 800, height: 600, show: true})
   onlineStatusWindow.loadURL(`file://${process.cwd()}/src/online-status.html`)
 })
 
